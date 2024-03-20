419c6732c9eb1b6d90bfa6f29699238fec8e95c7
Ihuoma Maduabuchukwu <melissagolden56@gmail.com>
2024-03-21 00:14:13 +0100

    Made port and hostname variables

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-
 const http = require("http");
+const port = 8900;
+const hostname = "localhost";
 
 const server = http.createServer( (req, res) => {
   res.write("Ihuoma Maduabuchukwu here!!");
   res.end();
 });
 
-server.listen(8900, "localhost", () => {
-  console.log("Server is running on port 8900");
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running on http://${hostname} ${port}`);
 });
